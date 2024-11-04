589a682a77af81daaffe72cd96ad2a7f07f41dd9
Ned Bellavance <ned1313@users.noreply.github.com>
2024-11-04 16:05:19 -0500

    [Resourcely Instance] Add storage bucket

--- main.tf
+++ main.tf
@@ -2,3 +2,21 @@ provider "google" {
     project = var.project_id
     region  = var.region
 }
+
+resource "google_storage_bucket" "neds-bucket-8679_p8Nhwe76WrML7b64" {
+  name                        = "neds-bucket-8679"
+  location                    = "US-CENTRAL1"
+  force_destroy               = true
+  public_access_prevention    = "enforced"
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    condition {
+      age = 7
+    }
+
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
