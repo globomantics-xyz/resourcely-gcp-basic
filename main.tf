01f17d7748d463206c5e7043e20a99d605aa3e38
Ned Bellavance <ned1313@users.noreply.github.com>
2024-11-08 15:05:51 -0500

    [Resourcely Instance] add-new-bucket

--- main.tf
+++ main.tf
@@ -2,3 +2,21 @@ provider "google" {
     project = var.project_id
     region  = var.region
 }
+
+resource "google_storage_bucket" "tacos-5555_SqvdTraJCKxXZ2Nb" {
+  name                        = "tacos-5555"
+  location                    = "US-CENTRAL1"
+  force_destroy               = true
+  public_access_prevention    = "inherited"
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    condition {
+      age = 7
+    }
+
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
